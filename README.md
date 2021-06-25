eb808b2a2d94404d1f293a129eee7f1f5885ebe8
Daniel Breen <daniel.breen@kno2.com>
2021-06-25 09:26:18 -0600

    Create Theme Package

---
+++ README.md
@@ -0,0 +1,2 @@
+# ngx-admin-theme
+A fork of [akveo/ngx-admin](https://github.com/akveo/ngx-admin) that includes only the theme.
---
+++ index.ts
@@ -0,0 +1,7 @@
+export * from './lib/theme.module';
+
+export * from './lib/components';
+// export * from './lib/directives';
+export * from './lib/layouts';
+export * from './lib/pipes';
+export * from './lib/styles';
